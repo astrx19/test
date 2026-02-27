436b49e097ec9c38fff1b159d4f6542465e8a990
TofuRoo <220467715+TofuRook@users.noreply.github.com>
2026-02-27 21:17:24 +0800

    ok ok ok ok

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("RAKAN");
-        
+        System.out.println("asdfhajsdkfhasdiu fhasdilufhas kljfbajksdlfbailufh");
     }
 }
